42866b52c81fec9dd724aa4ed8bd41e61664afa7
Luz Racca <luz@impraise.com>
2019-01-09 19:11:40 -0300

    Creation of a Single View App (Basic)

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Luz on 09/01/2019.
+//  Copyright © 2019 luz-ams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
